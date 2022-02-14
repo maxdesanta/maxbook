fcc0661c949b5241c365e0ced938fc23486c9cc4
maxdesanta <maxdesanta1@gmail.com>
2022-02-14 11:58:38 +0700

    toko buku back-end perubahan pertama

--- maxbook-backend/app.js
+++ maxbook-backend/app.js
@@ -1,8 +1,27 @@
 'use strict'
 
-// dotenv
+// Init dotenv
 require('dotenv').config();
 
-// bahan
-const express = express('express');
+// Init Express
+const express = require('express');
 const app = express();
+
+// init other modules
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// ini port
+const PORT = process.env.PORT || 3000;
+
+// cors setup
+let originOptionCors = {
+    origin: `http://localhost:${PORT}`
+}
+
+app.use('/', function (req, res) {
+    res.send('hello world');
+})
+
+// Port Connection
+app.listen(PORT, () => `connect to server on port ${PORT}`);
