0f4a66bf071a85ec0d64e5a8ca718e522d1b128a
maxdesanta <maxdesanta1@gmail.com>
2022-03-28 16:04:14 +0700

    react js perubahan pertama

---
+++ maxbook-frontend/src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default function Homepage() {
+    return (
+        <div>
+        <Header />
+        <main>
+            <h1>ini Homepage</h1>
+        </main>
+        <Footer />
+        </div>
+    )
+};
